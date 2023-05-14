db380fcde103a59f5415563c137c43eae3d20db7
Ilab Sentuser <ilabsentuser@gmail.com>
2023-05-14 20:50:25 +0200

    Implement some input handling to avoid issues with improper validation

--- app/controllers/PublicContentBase.java
+++ app/controllers/PublicContentBase.java
@@ -12,12 +12,28 @@ public class PublicContentBase extends Controller {
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
-        registerComplete();
+        //The following blocks verify if either the username is null, empty or contains characters that are not either letters or numbers
+        //If any of the aforementioned conditions occur then render an html with an error message
+        //Note, probably we could reuse the original register.html file and just pass the error as there is code to handle it, but since I am not profficient with this framework I will just go with an easier solution which is duplicating the register.html file and putting an error message on the new one.
+        if (username == null || username.length()==0) {
+            registerError();
+        } else {
+            for (char c : username.toCharArray()) {
+                if (!Character.isLetterOrDigit(c)) {
+                    registerError();
+                }
+                User u = new User(username, HashUtils.getMd5(password), type, -1);
+                u.save();
+                registerComplete();
+            }
+        }
     }
 
     public static void registerComplete(){
+        render();
+    }
+
+    public static void registerError(){
         render();
     }
 }
